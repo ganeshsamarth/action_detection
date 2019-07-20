8372552b2ff8b98c90ef3796b145c521d7d283fb
ganeshsamarth <47754795+ganeshsamarth@users.noreply.github.com>
2019-07-20 12:19:28 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -4,14 +4,25 @@ import os
 import cv2
 video_directory='/home/pbu/ActivityNet/Crawler/Kinetics/youtube_videos'
 data=pd.read_csv('/home/pbu/Downloads/kinetics_train/kinetics_train.csv')
+#data=pd.read_csv('C:\\Users\\ganeshsamarth\\Downloads\\kinetics_train.csv')
 labels_list=data['label']
+labels_list_no_space=list()
 youtube_id_list=data['youtube_id']
 num_examples=len(youtube_id_list)
 num_videos=200
 num_frames_per_video=40
 label_names=os.listdir(video_directory)
+for labels in labels_names:
+    labels_list_no_space.append(labels.replace(" ",""))
+    os.rename(video_directory+'/'+labels,video_directory+'/'+labels.replace(" ",""))
+#print(labels_list_no_space)
 
-for i,labels in enumerate(label_names):
+
+
+
+
+'''
+for i,labels in enumerate(labels_list_no_space):
     for videos in random.sample(os.listdir(video_directory+'/'+labels),num_videos):
         vidcap = cv2.VideoCapture(videos)
         frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -25,5 +36,4 @@ for i,labels in enumerate(label_names):
             success, image = vidcap.read()
             print('Read a new frame: ', success)
             count += 1
-
-
+'''
